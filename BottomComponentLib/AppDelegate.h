cdddbae8625bb4b30667f3a20991590ab420e376
周飞 <1174977961@qq.com>
2018-10-12 15:33:40 +0800

    init:初始化工程

---
+++ BottomComponentLib/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BottomComponentLib
+//
+//  Created by zhoufei on 2018/10/12.
+//  Copyright © 2018年 zhoufei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BottomComponentLib/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BottomComponentLib
+//
+//  Created by zhoufei on 2018/10/12.
+//  Copyright © 2018年 zhoufei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
