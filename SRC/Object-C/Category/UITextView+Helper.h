b7de97230229e23cf188743163c8ce1f48f31a2a
周飞 <1174977961@qq.com>
2018-10-23 10:12:02 +0800

    add:添加TextView分类

---
+++ SRC/Object-C/Category/UITextView+Helper.h
@@ -0,0 +1,16 @@
+//
+//  UITextView+Helper.h
+//  BottomComponentLib
+//
+//  Created by zhoufei on 2018/10/18.
+//  Copyright © 2018年 zhoufei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UITextView (Helper)
+/**
+ 文字上下剧中
+ */
+- (void)contentSizeToFit;
+@end
